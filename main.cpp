713da1712603c243d8b293bd5bd4a81069b81f78
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-05-19 11:44:24 +0900

    01_00完成

--- main.cpp
+++ main.cpp
@@ -18,10 +18,6 @@ struct Vector3 final {
 	float z;
 };
 
-static Matrix4x4 MakePerspectiveFovMatrix(float rad);
-static Matrix4x4 MakeOrthographicMatrix(float rad);
-static Matrix4x4 MakeViewportMatrix(float rad);
-
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
 	for (int row = 0; row < 4; ++row) {
 		for (int column = 0; column < 4; ++column) {
@@ -34,19 +30,38 @@ Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip
 
 	return Matrix4x4{
 		1 / aspectRatio * std::tan(rad) * (fovY / 2), 0, 0, 0,
-
-
+		0, std::tan(fovY / 2), 0, 0,
+		0, 0, farClip / farClip - nearClip, 1,
+		0, 0, -nearClip * farClip / (farClip - nearClip)
 	};
 
 }
 
-Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
 
-Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+	return Matrix4x4{
+		2 / right - left, 0, 0, 0,
+		0, 2 / top - bottom, 0, 0,
+		0, 0, 1 / farClip - nearClip, 0,
+		left + right / left - right, top + bottom / bottom - top, nearClip / nearClip - farClip, 1
+	};
+
+}
+
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+
+	return Matrix4x4{
+		width / 2, 0, 0, 0,
+		0, -height / 2 , 0, 0,
+		0, 0, maxDepth - minDepth, 0,
+		left + width / 2, top + height / 2, minDepth, 1
+	};
+
+}
 
 Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.f, 160.f, 200.0f, 300.0f, 0.0f, 1000.0f);
 
-Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f, 0.0f);
 
 Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
@@ -72,8 +87,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
-		
 
 		///
 		/// ↑更新処理ここまで
